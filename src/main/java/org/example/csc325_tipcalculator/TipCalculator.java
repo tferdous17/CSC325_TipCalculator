0dee51116901051a57b323a693bc3be0b82c80b5
Tasnim Ferdous <tferdous611@gmail.com>
2024-03-06 18:38:54 -0500

    refactor to reflect Deitel and Deitel source code

--- src/main/java/org/example/csc325_tipcalculator/TipCalculator.java
+++ src/main/java/org/example/csc325_tipcalculator/TipCalculator.java
@@ -11,13 +11,14 @@ public class TipCalculator extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TipCalculator.class.getResource("TipCalculator.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240); // attach scene graph to scene
+        stage.setTitle("Tip Calculator"); // displayed in window's title bar
+        stage.setScene(scene); // attach scene to stage
+        stage.show(); // display the stage
     }
 
     public static void main(String[] args) {
+        // create a TipCalculator object and call its start method
         launch();
     }
 }
